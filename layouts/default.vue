82affb96f076f3ac77f357930231cb88046d5e85
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-09-25 18:52:30 +0300

    Связь с Firebase - удачно!!!

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <v-app>
+      <div>
+        <p>123</p>
+      </div>
       <nuxt/>
     </v-app>
   </div>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-shop',
+    titleTemplate: '%s | nuxt-shop',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
